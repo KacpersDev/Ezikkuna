abc4c70a9b13a47d5f174d7bea3b95f86fa0ec3a
Kacper Mikolajczyk <vkacperpl@gmail.com>
2022-09-22 20:44:25 +0200

    Added every file

---
+++ js/main.js
@@ -0,0 +1,17 @@
+let clicked = false;
+
+const selectorClick = () => { 
+    if (!clicked) {
+        document.getElementById("click").style.visibility = 'visible';
+        clicked = true;
+        document.getElementById("selector_button").style.backgroundColor = 'rgb(57, 57, 57)';
+    } else {
+        document.getElementById("click").style.visibility = 'hidden';
+        clicked = false;
+        document.getElementById("selector_button").style.backgroundColor = 'white';
+    }
+};
+
+const selectorButtonClick = () => {
+    
+};
