c3fafceae191b885dd8047c8c6a1072e57532c19
Kacper Mikolajczyk <vkacperpl@gmail.com>
2022-10-01 00:15:30 +0200

    Added some responsive to footer in price.html

---
+++ js/price.js
@@ -0,0 +1,23 @@
+const first = 0;
+const second = 0;
+const third = 0;
+const fourth = 0;
+const fivth = 0;
+const sixth = 0;
+const seventh = 0;
+const eighth = 0;
+const ninth = 0;
+const tenth = 0;
+let total = 0;
+
+const add = (value) => {
+    total = total + value;    
+};
+
+const remove = (value) => {
+    total = total - value;
+}
+
+const getTotal = () =>{
+    return total;
+}
