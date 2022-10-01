e0455bc638a1dfde3b41d6309cbd11658545a3ab
Kacper Mikolajczyk <vkacperpl@gmail.com>
2022-10-01 16:24:48 +0200

    Added functions to first window

--- js/price.js
+++ js/price.js
@@ -1,4 +1,7 @@
-const first = 0;
+const first = {
+    price: 10,
+    time: 10,
+};
 const second = 0;
 const third = 0;
 const fourth = 0;
@@ -8,16 +11,33 @@ const seventh = 0;
 const eighth = 0;
 const ninth = 0;
 const tenth = 0;
-let total = 0;
 
-const add = (value) => {
-    total = total + value;    
+let firstClick = 1;
+let total = 0;
+let minutes = 0;
+
+const addFirst = () => {
+    total = total + first.price;
+    firstClick = firstClick + 1;
+    minutes = minutes + first.time;
+    
+    document.getElementById('value-1').innerHTML = firstClick;
+    document.getElementById('price_value').innerHTML = total + '$';
+    document.getElementById('time_value').innerHTML = minutes + ' mins';
 };
 
-const remove = (value) => {
-    total = total - value;
-}
+const removeFirst = () => {
+    if (total > 0) { 
+        total = total - first.price;
+    }
+    if (minutes > 0) {
+        minutes = minutes - first.time;
+    }
+    if (firstClick > 0) {
+        firstClick = firstClick - 1;
+    }
 
-const getTotal = () =>{
-    return total;
+    document.getElementById('value-1').innerHTML = firstClick;
+    document.getElementById('price_value').innerHTML = total + '$';
+    document.getElementById('time_value').innerHTML = minutes + ' mins';
 }
